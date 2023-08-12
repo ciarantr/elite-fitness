b89a259fa48dc659d9c7762bb144a4ced514b514
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-08-12 14:22:38 +0100

    feat: Add product attributes models [issue-8]
    Added two new models, AttributeType and Attribute, to keep track of the
    various attributes for products in the ecommerce app, such as color or
    size.

--- apps/ecommerce/products/models.py
+++ apps/ecommerce/products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+class AttributeType(models.TextChoices):  # enum class
+    """Attribute type model"""
+    COLOR = 'color', 'Color'
+    SIZE = 'size', 'Size'
+    WEIGHT = 'weight', 'Weight'
+    COUNT = 'count', 'Count'
+
+
+class Attribute(models.Model):
+    class Meta:
+        verbose_name_plural = 'Product attributes'
+
+    type = models.CharField(max_length=50,
+                            verbose_name='Attribute type',
+                            choices=AttributeType.choices)
+
+    name = models.CharField(max_length=50,
+                            unique=True,
+                            verbose_name='Attribute name',
+                            help_text='Required. 50 characters or fewer.')
+
+    def __str__(self):
+        return f'{self.type} - {self.name}'
+
+
 # Create your models here.
