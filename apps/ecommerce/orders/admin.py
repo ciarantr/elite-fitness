575ca928a42dfe2ca3d5c47d65a374774a597907
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-08-15 22:56:27 +0100

    feat: add admin interface for Order models
    - Added Django Admin classes for Order and OrderLineItem models to
    provide a user interface for these models in the admin site. The
    OrderAdmin class also includes the setting for display fields, read-only
    fields, list display fields, and ordering parameter for sorting.

--- apps/ecommerce/orders/admin.py
+++ apps/ecommerce/orders/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderLineItem
+
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_cart',
+                       'stripe_pid',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total', 'original_cart',
+              'stripe_pid',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
