a116f97464d24a4a81c4dd09033183a18e277d9d
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-08-15 23:27:41 +0100

    feat: add subscription form to home page
    - Added a subscription form to the home view. It handles the
    post-request. If the form is valid and there are no existing
    subscriptions with the provided email, the subscription is saved and
    the user is redirected back to the home page. If the form is not valid,
    the errors are added to the context and sent back to the home view.

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 from apps.ecommerce.products.models import Product
+from apps.subscriptions.forms import SubscriptionForm
+from apps.subscriptions.models import Subscription
 
 
 class HomePageView(TemplateView):
@@ -13,5 +16,20 @@ class HomePageView(TemplateView):
         # Assign image to each product
         for product in context['products']:
             product.image = product.images.first()
+        # Add a subscription form
+        context['form'] = SubscriptionForm()
 
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            if not Subscription.objects.filter(email=email).exists():
+                form.save()
+                return redirect('home')
+        else:
+            # add form errors to context
+            context = self.get_context_data(**kwargs)
+            context['form'] = form
+            return self.render_to_response(context)
