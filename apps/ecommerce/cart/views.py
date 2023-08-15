e5da5dee6c0363e1dfd2a12ac80feef56907ab51
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-08-15 23:26:03 +0100

    feat(cart): implement add, remove, edir cart items
    - Implemented `AddToCartView` for handling addition of items into the
    cart
    - Implemented `RemoveFromCartView` for dealing with removal of items
    from the cart
    - Implemented `AdjustCartView` for adjusting the quantity of an item in
    the cart
    
    Each class uses Django views to handle POST requests modifying the cart
    session. Also, user messages have been added to provide feedback for
    each operation.

--- apps/ecommerce/cart/views.py
+++ apps/ecommerce/cart/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, reverse
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+from apps.ecommerce.products.models import Product
+
+
+class CartPageView(TemplateView):
+    template_name = 'cart.html'
+
+
+class AddToCartView(View):
+    def post(self, request, item_id):
+        product = Product.objects.get(pk=item_id)
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+
+        cart = request.session.get('cart', {})
+
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+            messages.success(request,
+                             f'Updated {product.name} quantity to '
+                             f'{cart[item_id]}')
+        else:
+            cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
+
+        request.session['cart'] = cart
+        return redirect(redirect_url)
+
+
+class RemoveFromCartView(View):
+    def post(self, request, item_id):
+        try:
+            product = get_object_or_404(Product, pk=item_id)
+
+            cart = request.session.get('cart', {})
+
+            cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your cart')
+
+            request.session['cart'] = cart
+            return redirect(reverse('cart'))
+
+        except Exception as e:
+            messages.error(request, f'Error removing item: {e}')
+            return HttpResponse(status=500)
+
+
+class AdjustCartView(View):
+    def post(self, request, item_id):
+        product = get_object_or_404(Product, pk=item_id)
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+
+        cart = request.session.get('cart', {})
+
+        if quantity > 0:
+            cart[item_id] = quantity
+            messages.success(request, f'Updated {product.name}'
+                                      f' quantity to {cart[item_id]}')
+        else:
+            cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your cart')
+
+        request.session['cart'] = cart
+        return redirect(redirect_url)
+
