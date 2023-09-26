78f195260ccfe0d064ed2a5ffd8cf8834e4ecd21
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-26 12:22:30 +0100

    feat: add subscription confirmation via email
    A confirmation email functionality was added to the subscription process.
    Now when a user submits the subscription form, a confirmation email is
    triggered. This email carries a unique confirmation link for the user.
    This change ensures that the users can confirm their interest and also
    helps reduce spurious signups.

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,9 +1,38 @@
+from django.contrib import messages
+from django.core.mail import send_mail
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 from apps.ecommerce.products.models import Product
 from apps.subscriptions.forms import SubscriptionForm
 from apps.subscriptions.models import Subscription
+from core import settings
+
+
+def _send_confirmation_email(form, confirmation_link):
+    """Send the user a confirmation email"""
+    email = form.email
+    first_name = form.first_name
+
+    subject = render_to_string(
+        'emails/confirmation_subscription_subject.txt',
+    )
+    # Render the template to a string and escape the HTML content
+    body = render_to_string(
+        'emails/confirmation_subscription_body.html',
+        {'first_name': first_name,
+         'confirmation_link': confirmation_link},
+    )
+    send_mail(
+        subject=subject,
+        message=False,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[email],
+        html_message=body,
+        fail_silently=False,
+    )
 
 
 class HomePageView(TemplateView):
@@ -22,14 +51,29 @@ class HomePageView(TemplateView):
         return context
 
     def post(self, request, *args, **kwargs):
+        """
+        Save the subscription form if valid & user email does not exist.
+        Send confirmation email to user.
+        """
         form = SubscriptionForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
+
             if not Subscription.objects.filter(email=email).exists():
                 form.save()
-                return redirect('home')
-        else:
-            # add form errors to context
-            context = self.get_context_data(**kwargs)
-            context['form'] = form
-            return self.render_to_response(context)
+                confirmation_link = request.build_absolute_uri(
+                    reverse('confirm-signup',
+                            kwargs={
+                                'confirmation_token':
+                                    form.instance.confirmation_token
+                            }
+                            )
+                )
+
+                _send_confirmation_email(form.instance, confirmation_link)
+
+        messages.info(request,
+                      f'''Please check your email for to
+                          confirm subscription.''')
+        return redirect('home')
+
