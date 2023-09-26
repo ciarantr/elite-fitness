89552013a076fd4ca1191db0c60daf7355f9d86f
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-26 13:48:52 +0100

    feat: add post_save signal to AccountsConfig
    Added a Django post_save signal to the AccountsConfig class in the
    apps/accounts/apps.py file. This signal calls the update_email_address
    function from apps.accounts.signals whenever the auth.User model is
    saved. This ensures that changes to a user's email address are properly
    updated in the system.

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.accounts'
+
+    def ready(self):
+        from apps.accounts.signals import update_email_address
+        post_save.connect(update_email_address, sender='auth.User')
