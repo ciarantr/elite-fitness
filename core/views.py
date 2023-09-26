8fdbb87e838a4c42a3fe414458c5b3812bdfb5c4
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-26 12:53:51 +0100

    feat: add error views & templates for 404/500
    1. Added error views/templates to handle 404/500 page errors.

---
+++ core/views.py
@@ -0,0 +1,45 @@
+from django.views.generic import TemplateView
+
+
+class Handler404View(TemplateView):
+    """
+    A class based view for the custom 404 error page
+    """
+
+    template_name = 'errors/base_error.html'
+
+    # send 404 status code
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        response.status_code = 404
+        return response
+
+    # Add meta title, message and status code
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '(404) Whoops! We Can’t Find That Page'
+        context['message'] = 'The page you\'re looking for could not be found.'
+        context['status_code'] = '404'
+        return context
+
+
+class Handler500View(TemplateView):
+    """
+    A class based view for the custom 500 error page
+    """
+
+    template_name = 'errors/base_error.html'
+
+    # send 500 status code
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        response.status_code = 500
+        return response
+
+    # Add meta title, message and status code
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '(500) Whoops! We Messed Up.'
+        context['message'] = 'Whoops! We Messed Up.'
+        context['status_code'] = '500'
+        return context
