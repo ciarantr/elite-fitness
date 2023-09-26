12b90276b4f0950ac1b36c1a0d710cb8e6b5969d
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-26 13:36:03 +0100

    refactor: update account views, add order view
    Simplified the account views by removing the custom login, registration,
    and logout forms and views. Switched from using a custom user profile
    model to the default Django User model. Introduced a view and form for
    delivery details to enhance the ordering process. These changes
    streamline the authentication process, simplify the code, and
    facilitate user profile and delivery information management.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,53 +1,48 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
-from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import UpdateView
 
-from core import settings
-from .forms import CustomLoginForm, CustomUserCreationForm
-from .forms import CustomerProfileForm
-from .models import CustomerProfile
+from .forms import CustomerDeliveryForm, UserUpdateForm
+from .models import DeliveryDetails
 
 
-class AccountLoginView(LoginView):
-    form_class = CustomLoginForm
-    template_name = 'login.html'
-    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
-    redirect_authenticated_user = True
-
-
-class AccountRegisterView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = settings.LOGIN_REDIRECT_URL
-    template_name = 'register.html'
-
-    def dispatch(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect('home')
-        return super().dispatch(*args, **kwargs)
-
-
-class AccountLogoutView(LogoutView):
-    next_page = settings.LOGOUT_REDIRECT_URL
-
-
-class CustomerProfileView(LoginRequiredMixin, UpdateView):
+class ProfileView(LoginRequiredMixin, UpdateView):
+    """
+    This view is responsible for displaying the user's profile information
+    """
     template_name = 'profile.html'
-    model = CustomerProfile
-    form_class = CustomerProfileForm
-    success_url = '/profile/'
+    form_class = UserUpdateForm
+    success_url = reverse_lazy('profile')
     context_object_name = 'profile'
 
     def get_object(self, queryset=None):
-        return get_object_or_404(CustomerProfile, user=self.request.user)
+        #     return logged in user
+        return get_object_or_404(User, pk=self.request.user.pk)
 
     def form_valid(self, form):
-        response = super().form_valid(form)
         messages.success(self.request, 'Profile updated successfully')
-        return response
+        return super().form_valid(form)
+
+
+class OrderProfileView(LoginRequiredMixin, UpdateView):
+    template_name = 'delivery_information.html'
+    model = DeliveryDetails
+    form_class = CustomerDeliveryForm
+    success_url = reverse_lazy('order_information')
+    context_object_name = 'profile'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(
+            DeliveryDetails.objects.prefetch_related('orders'),
+            user=self.request.user)
+
+    def form_valid(self, form):
+        messages.success(self.request,
+                         'Delivery information updated successfully')
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
