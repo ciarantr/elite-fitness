05283411cd005a7c2e0578d6d5793e47018c45bc
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-27 09:14:17 +0100

    refactor: adjust cart functionality
    1. Modified cart functionality to handle out-of-stock product instances,
    and implemented a default image placeholder for products with no images.
    2. Added a list of exclusion paths to prevent cart from populating on
    certain routes.

--- apps/ecommerce/cart/context_processors.py
+++ apps/ecommerce/cart/context_processors.py
@@ -11,9 +11,10 @@ def cart_processor(request):
     subtotal = 0
     grand_total = 0
     cart_items = []
+    path = request.path
 
     for item_id, count in cart.items():
-        product =  get_object_or_404(Product, pk=item_id)
+        product = get_object_or_404(Product, pk=item_id)
         max_stock = product.stock
         actual_quantity = min(count, max_stock)
         cart_items.append({
@@ -22,7 +23,10 @@ def cart_processor(request):
             'price': product.price,
             'total': product.price * actual_quantity,
             'stock': max_stock,
-            'image': product.images.first().image if product.images.count() > 0 else None,
+            # get the first image of the product or media placeholder.svg
+            'image': product.images.first().image.url
+            if product.images.count() > 0
+            else settings.MEDIA_URL + 'products/placeholder.svg',
         })
         total_quantity += actual_quantity
         subtotal += product.price * actual_quantity
@@ -36,12 +40,27 @@ def cart_processor(request):
         delivery_cost = settings.STANDARD_DELIVERY_COST
         grand_total += delivery_cost
 
+    excluded_paths = [
+        '/cart/',
+        '/account/profile/',
+        '/checkout/',
+        '/wishlist/',
+        '/account/confirm-email/',
+        '/support/contact/',
+        'subscriptions/confirm-signup/<uuid:confirmation_token>/',
+        'subscriptions/confirm-signup/94bee8f-97d2-4fc1-accf-fc61610a787b/',
+    ]
+
+    exclude_path = any(path.startswith(excluded_path) for excluded_path in
+                       excluded_paths)
+
     context = {
         'cart_items': cart_items,
         'cart_total_quantity': total_quantity,
         'delivery_cost': delivery_cost,
         'subtotal': subtotal,
         'grand_total': grand_total,
+        'is_excluded_path': exclude_path,
     }
 
     return context
