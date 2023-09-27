31684e648c8a2a90ef9c3c86b8de9ee821b6f3b0
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-27 17:29:41 +0100

    refactor: url pattern
    1. Use hyphenated url structure for app consistency.

--- apps/ecommerce/checkout/urls.py
+++ apps/ecommerce/checkout/urls.py
@@ -4,11 +4,10 @@ from .views import CacheCheckoutDataView, CheckoutSuccessView, CheckoutView
 from .webhooks import webhook
 
 urlpatterns = [
-
     path('', CheckoutView.as_view(), name='checkout'),
-    path('checkout_success/<str:order_number>/', CheckoutSuccessView.as_view(),
+    path('checkout-success/<str:order_number>/', CheckoutSuccessView.as_view(),
          name='checkout_success'),
-    path('cache_checkout_data/', CacheCheckoutDataView.as_view(),
+    path('cache-checkout-data/', CacheCheckoutDataView.as_view(),
          name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
