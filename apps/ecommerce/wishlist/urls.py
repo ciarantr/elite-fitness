9fa2bbd33178647f458d4d06f04e6a60d3eeed83
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-27 10:09:06 +0100

    feat: add wishlist urls and base files
    1. Add urls to for viewing & updating users wishlist.
    2. Add migration files.
    3. Add base wishlist app files
    
    issue: #16

---
+++ apps/ecommerce/wishlist/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from .views import (WishListDetailView, WishListView, WishlistAddProductView,
+                    WishlistCreateView, WishlistDeleteFormView,
+                    WishlistRemoveProductView, WishlistUpdateView)
+
+urlpatterns = [
+    path('wishlist/', WishListView.as_view(), name='wishlist'),
+    path('wishlist/details/<int:pk>/', WishListDetailView.as_view(),
+         name='wishlist_details'),
+    path('wishlist/create/', WishlistCreateView.as_view(),
+         name='wishlist_create'),
+    path('wishlist/edit/<int:pk>/', WishlistUpdateView.as_view(),
+         name='wishlist_update'),
+    path('wishlist/delete/<int:pk>', WishlistDeleteFormView.as_view(),
+         name='wishlist_delete'),
+    path('wishlist/add/<int:product_id>', WishlistAddProductView.as_view(),
+         name='wishlist_product_add'),
+    path('wishlist/<int:list_id>/product-remove/<int:product_id>/',
+         WishlistRemoveProductView.as_view(),
+         name='wishlist_product_remove'),
+]
