1d8749871419c0a6444ae5150424a9ac8b1ad9ff
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-27 09:29:27 +0100

    feat: add wishlist form to cart view
    1. Add wishlist form in the context of the CartPageView if a user is authenticated. This gives users the ability to add items to a wishlist
    directly from the cart page.
    
    2. Add default quantity for the AddToCartView was also enforced to
    manage cases where no quantity is specified. The redirect path for
    RemoveFromCartView was also updated to redirect to a variable path,
    enhancing site navigation.
    
    issue: #16

--- apps/ecommerce/cart/views.py
+++ apps/ecommerce/cart/views.py
@@ -1,20 +1,41 @@
 from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, reverse
+from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 from django.views.generic import TemplateView
 
 from apps.ecommerce.products.models import Product
+from apps.ecommerce.wishlist.forms import AddProductToWishlistForm
+from apps.ecommerce.wishlist.models import List
 
 
 class CartPageView(TemplateView):
+    """
+    This view handles the display of the cart page
+    """
+
     template_name = 'cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context['wishlist_exists'] = List.objects.filter(
+                user=self.request.user).exists()
+            context['add_to_wishlist_form'] = AddProductToWishlistForm(
+                user=self.request.user)
+        return context
 
 
 class AddToCartView(View):
+    """
+    This view handles the addition of a product to the cart
+    """
+
     def post(self, request, item_id):
         product = Product.objects.get(pk=item_id)
-        quantity = int(request.POST.get('quantity'))
+        quantity = int(request.POST.get('quantity')) if request.POST.get(
+            'quantity') else 1
         redirect_url = request.POST.get('redirect_url')
 
         cart = request.session.get('cart', {})
@@ -33,6 +54,10 @@ class AddToCartView(View):
 
 
 class RemoveFromCartView(View):
+    """
+    This view handles the removal of a product from the cart
+    """
+
     def post(self, request, item_id):
         try:
             product = get_object_or_404(Product, pk=item_id)
@@ -43,7 +68,9 @@ class RemoveFromCartView(View):
             messages.success(request, f'Removed {product.name} from your cart')
 
             request.session['cart'] = cart
-            return redirect(reverse('cart'))
+            redirect_url = request.POST.get('redirect_path')
+
+            return redirect(redirect_url)
 
         except Exception as e:
             messages.error(request, f'Error removing item: {e}')
@@ -51,6 +78,10 @@ class RemoveFromCartView(View):
 
 
 class AdjustCartView(View):
+    """
+    This view handles the adjustment of a product quantity in the cart
+    """
+
     def post(self, request, item_id):
         product = get_object_or_404(Product, pk=item_id)
         quantity = int(request.POST.get('quantity'))
@@ -68,4 +99,3 @@ class AdjustCartView(View):
 
         request.session['cart'] = cart
         return redirect(redirect_url)
-
