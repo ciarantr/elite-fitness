88d79e241aa58c51c99b4ef7f18f83e3388b98a0
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-27 10:17:24 +0100

    feat: add wishlist editing form
    1. Form allows users to edit an existing wishlist in the app. The form
    includes `name` and `description` fields, with the option for the user
    to make them optional. Also, autofocus was set on the first field to
    improve user experience.
    
    issue:#16

--- apps/ecommerce/wishlist/forms.py
+++ apps/ecommerce/wishlist/forms.py
@@ -41,3 +41,38 @@ class CreateWishlistForm(forms.ModelForm):
             raise ValidationError('A wishlist with this name already exists.'
                                   ' Please choose another.')
         return name
+
+
+class EditWishlistForm(forms.ModelForm):
+    """
+    Form for editing a wishlist
+    """
+
+    class Meta:
+        model = List
+        fields = ['name', 'description']
+        widgets = {
+            'description': Textarea(attrs={'cols': 20, 'rows': 5}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+
+        placeholders = {field: self.Meta.model._meta.get_field(
+            field).verbose_name for field in self.fields}
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'placeholder': placeholders.get(field),
+            })
+            if self.Meta.model._meta.get_field(field).blank:
+                self.fields[field].widget.attrs['placeholder'] += ' (optional)'
+            self.fields[field].label = False
+
+
