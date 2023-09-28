657d8b3b4b1527c9213134816c76887efc92050d
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-28 10:43:01 +0100

    feat: add fields for email verification
    Two new fields were added to the Subscription model -
    'confirmation_token' and 'confirmed_at'. The 'confirmation_token' is a
    unique UUID generated dynamically and will be used to verify the
    subscription by the user. The 'confirmed_at' will store the date and
    time when the user verifies their subscription. These changes were made
    to enhance the security measures of our subscription system.
    
    issue:#15

--- apps/subscriptions/models.py
+++ apps/subscriptions/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 
@@ -11,6 +13,9 @@ class Subscription(models.Model):
     email = models.EmailField(max_length=254, unique=True, )
     created = models.DateTimeField(auto_now_add=True, editable=False,)
     updated = models.DateTimeField(auto_now=True)
+    confirmation_token = models.UUIDField(default=uuid.uuid4, editable=False,
+                                          null=True)
+    confirmed_at = models.DateTimeField(null=True, blank=True)
     is_verified = models.BooleanField(default=False)
 
     def __str__(self):
