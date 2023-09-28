d70c9092aa2c6161e95c93062c2b8c4a2339fe87
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-09-28 10:40:43 +0100

    feat: add email confirmation functionality in signup
    1. Added a ConfirmSignupView in the subscriptions app that confirms a
    user's email once they click on the confirmation link sent to them. The
    view checks for a valid confirmation_token in the Subscription model and
    updates the respective record by setting is_verified to True, setting
    the confirmed_at field to the current time, and subsequently removing
    the confirmation_token. This will prevent the re-use of the
    confirmation link. This was necessary to ensure user accounts that sign
    up for the newsletter are valid and controlled by the user.
    
    issue#15

--- apps/subscriptions/views.py
+++ apps/subscriptions/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.utils import timezone
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Subscription
+
+
+class ConfirmSignupView(TemplateView):
+    """
+    Confirm the user's email address
+    when they click on the link in the email from newsletter (homepage)
+    """
+    template_name = 'signup_confirmed.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        confirmation_token = kwargs.get('confirmation_token')
+
+        try:
+            subscription = Subscription.objects.get(
+                confirmation_token=confirmation_token)
+            subscription.is_verified = True
+            # set the confirmed_at date
+            subscription.confirmed_at = timezone.now()
+            # remove the uuid so the user can't use the link again
+            subscription.confirmation_token = None
+            subscription.save()
+            full_name = f'{subscription.first_name} {subscription.last_name}'
+            context.update({
+                'full_name': full_name,
+                'email': subscription.email
+            })
+
+        except Subscription.DoesNotExist:
+            raise Http404
+
+        return context
