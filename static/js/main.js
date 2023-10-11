39a425d1dca85c6db04319465e831230383a3a4a
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-10-11 13:17:36 +0100

    refactor ARIA control methods and add resize event listener
    Changes were made to refactor and improve the readability of ARIA
    control methods. The setAria method was refactored for better ARIA
    management. Ensuring the values of attributes 'aria-expanded' and
    'aria-hidden' are being correctly set based on their contextual use
    cases. A resize window event listener was also added to handle menu
    states (open/close) in response to window resize events, improving the
    user experience on devices with varying screen sizes.

--- static/js/main.js
+++ static/js/main.js
@@ -10,8 +10,12 @@ const desktopSearchCloseBtn = desktopSearchContainer.querySelector(
 
 // Helper methods to control ARIA attributes and class
 const setAria = (element, elementTwo, expanded, hidden) => {
-  element.setAttribute('aria-expanded', expanded)
-  elementTwo.setAttribute('aria-hidden', hidden)
+  if (!expanded) {
+    elementTwo.setAttribute('aria-hidden', hidden)
+  } else {
+    element.setAttribute('aria-expanded', expanded)
+    elementTwo.setAttribute('aria-hidden', hidden)
+  }
 }
 
 const toggleClass = (element, className, action) => {
@@ -23,6 +27,15 @@ mobileMenuBtn.addEventListener('click', () => {
   toggleClass(mobileMenu, 'open', 'toggle')
   setAria(mobileMenuBtn, mobileMenu, 'true', 'false')
   setAria(closeMobileMenuBtn, mobileMenu, 'true', 'false')
+
+  // make sure the menu is closed when the window is resized
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 1024) {
+      mobileMenu.classList.remove('open')
+      mobileMenu.setAttribute('aria-hidden', 'true')
+      closeMobileMenuBtn.setAttribute('aria-expanded', 'false')
+    }
+  })
 })
 
 closeMobileMenuBtn.addEventListener('click', () => {
@@ -35,52 +48,38 @@ closeMobileMenuBtn.addEventListener('click', () => {
 // Set aria-attributes and toggle class on mouseover and mouseleave
 desktopMenu.forEach((menu) => {
   menu.addEventListener('mouseover', () => {
-    setAria(menu, menu.nextElementSibling, 'true', 'false')
+    setAria(menu, menu.nextElementSibling, false, 'false')
     toggleClass(menu.nextElementSibling, 'open', 'add')
   })
 
   menu.addEventListener('mouseleave', () => {
-    setAria(menu, menu.nextElementSibling, 'false', 'true')
+    setAria(menu, menu.nextElementSibling, false, 'true')
     toggleClass(menu.nextElementSibling, 'open', 'remove')
   })
 
   menu.nextElementSibling.addEventListener('mouseenter', () => {
-    setAria(menu, menu.nextElementSibling, 'true', 'false')
+    setAria(menu, menu.nextElementSibling, false, 'false')
     toggleClass(menu.nextElementSibling, 'open', 'add')
   })
 
   menu.nextElementSibling.addEventListener('mouseleave', () => {
-    setAria(menu, menu.nextElementSibling, 'false', 'true')
+    setAria(menu, menu.nextElementSibling, false, 'true')
     toggleClass(menu.nextElementSibling, 'open', 'remove')
   })
-})
-
-// make sure the menu is closed when the window is resized
-window.addEventListener('resize', () => {
-  if (window.innerWidth > 1024) {
-    mobileMenu.classList.remove('open')
-    mobileMenu.setAttribute('aria-hidden', 'true')
-    closeMobileMenuBtn.setAttribute('aria-expanded', 'false')
-  }
 })
 
 // Control the search form on desktop
 // Set aria-attributes and toggle class on click
 desktopSearchBtn.addEventListener('click', () => {
   desktopSearchContainer.classList.toggle('open')
-  const searchBtnAttrsState = desktopSearchBtn.getAttribute('aria-expanded')
+
   const desktopSearchBtnAttrsState =
     desktopSearchContainer.getAttribute('aria-hidden')
 
-  desktopSearchBtn.setAttribute(
-    'aria-expanded',
-    searchBtnAttrsState === 'true' ? 'false' : 'true',
-  )
   desktopSearchContainer.setAttribute(
     'aria-hidden',
     desktopSearchBtnAttrsState === 'true' ? 'false' : 'true',
   )
-
   // add focus to the search input
   desktopSearchContainer.querySelector('input').focus()
 })
@@ -92,41 +91,43 @@ desktopSearchCloseBtn.addEventListener('click', () => {
   desktopSearchBtn.setAttribute('aria-expanded', 'false')
 })
 
-const cartNavigation = document.querySelector('#cart-navigation');
-const cartDropdown = document.querySelector('#cart-dropdown');
+const cartNavigation = document.querySelector('#cart-navigation')
+const cartDropdown = document.querySelector('#cart-popover')
 
 function showCartDropdown() {
-  cartNavigation.setAttribute('aria-expanded', 'true');
-  cartDropdown.setAttribute('aria-hidden', 'false');
+  cartDropdown.setAttribute('aria-hidden', 'false')
 }
 function hideCartDropdown() {
-  if (cartDropdown.getAttribute('aria-hidden') === 'true' && cartNavigation.getAttribute('aria-expanded') === 'true') {
-    cartNavigation.setAttribute('aria-expanded', 'false');
-    cartDropdown.setAttribute('aria-hidden', 'true');
+  if (
+    cartDropdown.getAttribute('aria-hidden') === 'true' &&
+    cartNavigation.getAttribute('aria-expanded') === 'true'
+  ) {
+    cartNavigation.setAttribute('aria-expanded', 'false')
+    cartDropdown.setAttribute('aria-hidden', 'true')
   }
 }
 
-
 // Add event listeners for hover effects
-cartNavigation?.addEventListener('mouseover', showCartDropdown);
-cartNavigation?.addEventListener('mouseleave', hideCartDropdown);
-cartDropdown?.addEventListener('mouseleave', () => cartDropdown.setAttribute('aria-hidden', 'true'));
+cartNavigation?.addEventListener('mouseover', showCartDropdown)
+cartNavigation?.addEventListener('mouseleave', hideCartDropdown)
+cartDropdown?.addEventListener('mouseleave', () =>
+  cartDropdown.setAttribute('aria-hidden', 'true'),
+)
 
 setTimeout(() => {
-  const messages = document.querySelectorAll('[data-django-message]');
+  const messages = document.querySelectorAll('[data-django-message]')
   // Apply a fade-out animation using CSS transitions
   if (messages) {
-  //   loop through all the messages and add the fade-out class
+    //   loop through all the messages and add the fade-out class
     messages.forEach((element) => {
       // Apply a fade-out animation using CSS transitions
-      element.style.transition = 'opacity 0.5s';
-      element.style.opacity = '0';
+      element.style.transition = 'opacity 0.5s'
+      element.style.opacity = '0'
 
       // Remove the messages element from the DOM after the animation completes
       setTimeout(() => {
-        element.parentNode.removeChild(element);
-      }, 500);
-    }
-    );
+        element.parentNode.removeChild(element)
+      }, 500)
+    })
   }
-}, 4000);
+}, 4000)
