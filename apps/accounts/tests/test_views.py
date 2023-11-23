0b2120e8103e8c11c181ac03b339cc7a4f3aacac
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-11-23 19:10:42 +0000

    test(accounts): add tests for OrderProfile view
    Tests ensure that the OrderProfile view is
    behaving as expected. Specifically, tests are added to confirm that the
    view URL is correctly located with a 200 response code and is using the
    correct delivery information template.
    
    issue:#6

--- apps/accounts/tests/test_views.py
+++ apps/accounts/tests/test_views.py
@@ -54,3 +54,23 @@ class ProfileViewTest(TestCase):
                          'Profile updated successfully')
 
 
+class OrderProfileViewTest(TestCase):
+    """
+    Test the order profile view and form
+    """
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser',
+                                             password='12345')
+        self.login = self.client.login(username='testuser',
+                                       password='12345')
+        self.user.deliverydetails.default_full_name = "John Doe"
+        self.user.deliverydetails.save()
+        self.delivery_details_obj = DeliveryDetails.objects.get(id=1)
+
+    def test_view_url_exists_at_desired_location(self):
+        # Test that the view url exists at the desired location
+        # and returns a 200 response code & uses the correct template
+        response = self.client.get(reverse('order_information'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'delivery_information.html')
