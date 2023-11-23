ada8b8d0e8015fa3016d200ed2186de116b1bc55
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-11-23 19:08:21 +0000

    test(accounts): add test for profile update form
    Tests for the ProfileView by checking if the
    profile update form works as expected.
    Verifies the form's validity and the successful update of user profile
    details, including username, email, first name and last name.
    
    The test also checks if the appropriate success message is displayed
    after the profile is updated successfully.
    Additional imports were added: `DeliveryDetails` model and
    `get_messages` function.
    
    issue:#6

--- apps/accounts/tests/test_views.py
+++ apps/accounts/tests/test_views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
+from django.contrib.messages import get_messages
 from django.test import Client, TestCase
 from django.urls import reverse
+
+from ..models import DeliveryDetails
 
 
 class ProfileViewTest(TestCase):
@@ -24,3 +27,30 @@ class ProfileViewTest(TestCase):
         response = self.client.get(reverse('profile'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'profile.html')
+
+    def test_form_valid(self):
+        # Test that the form is valid and the user profile details are updated
+        # Test that the correct message is displayed
+        # Assuming form fields for `UserUpdateForm`
+        # are 'username', 'email', 'first_name', and 'last_name'
+        # Note: form used django auth User model
+
+        response = self.client.post(reverse('profile'), {
+            'username': 'newusername',
+            'email': 'user@gmail.com',
+            'first_name': 'newfirstname',
+            'last_name': 'newlastname',
+        }
+                                    )
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, 'newusername')
+        self.assertEqual(self.user.email, 'user@gmail.com')
+        self.assertEqual(self.user.first_name, 'newfirstname')
+        self.assertEqual(self.user.last_name, 'newlastname')
+
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]),
+                         'Profile updated successfully')
+
+
