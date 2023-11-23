22895316a196bb1acac8ac1ce701c3967f454faa
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-11-23 17:50:41 +0000

    fix: update core settings middleware
    1. Added django allauth account middleware

--- core/settings.py
+++ core/settings.py
@@ -87,6 +87,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # allauth account middleware:
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 AUTHENTICATION_BACKENDS = [
