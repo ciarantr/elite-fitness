da8016ef4281362858207621f8d7c001e2b4e080
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-11-24 08:18:35 +0000

    test(views): add tests for ContactView
    1.Test contact form submission, both successful and failed submissions.
    2. Test correct message is sent to user on successful/unsuccessful
    form submission.
    3.Test correct template, response code & page title is correct.
    
    issue:#6

--- apps/customer_support/tests/test_views.py
+++ apps/customer_support/tests/test_views.py
@@ -1,3 +1,7 @@
+import json
+from unittest.mock import mock_open, patch
+
+from django.contrib.messages import get_messages
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -22,3 +26,64 @@ class AboutViewTest(TestCase):
 
         self.assertIn('title', response.context_data)
         self.assertEqual(response.context_data['title'], 'About Us')
+
+
+class ContactViewTest(TestCase):
+    """
+    Test the ContactView
+    1. Test that the view url exists at the desired location
+    and returns a 200 response code & uses the correct template
+    2. Test that the view returns the correct context data
+    3. Test that the form is valid and mail is sent
+    4. Test that the form is invalid
+    """
+
+    def setUp(self):
+        self.client = Client()
+
+        self.valid_data = {
+            "first_name": "Test",
+            "last_name": "User",
+            "email": "test.user@gmail.com",
+            "phone_number": "1234567890",
+            "message": "Test message"
+        }
+
+    def test_get_context_data(self):
+        response = self.client.get(reverse('customer_support:contact'))
+
+        # Ensure status code 200 i.e., OK
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
+
+        self.assertIn('title', response.context_data)
+        self.assertEqual(response.context_data['title'], 'Contact Us')
+
+    # Test a when form is valid & mail is sent
+    @patch('apps.customer_support.views.send_mail')
+    def test_form_valid(self, mock_send_mail):
+        response = self.client.post(reverse('customer_support:contact'),
+                                    data=self.valid_data)
+
+        # Test that a success message has been added to messages
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Thank you Test for contacting us. '
+                         'We will get back to you shortly.')
+
+        # Test sending of mail
+        self.assertTrue(mock_send_mail.called)
+
+    # Test a when form is invalid
+    def test_form_invalid(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data['email'] = 'not an email address'
+
+        response = self.client.post(reverse('customer_support:contact'),
+                                    data=invalid_data)
+
+        # Test that an error message has been added to messages
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Please correct the errors below.')
+
