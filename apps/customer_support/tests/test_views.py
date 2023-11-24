d85dfb0ed67857fa6e6644fcac928dd62c732873
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-11-24 08:24:53 +0000

    test(views): add tests for ShippingAndInformationView
    1. Added tests for the following
        - existence of the view url at the desired location
        - statusCode returned being 200
        - correct template usage
        - correct context data
    
    issue:#6

--- apps/customer_support/tests/test_views.py
+++ apps/customer_support/tests/test_views.py
@@ -142,3 +142,31 @@ class PrivacyPolicyViewTest(TestCase):
                          'Privacy Policy')
 
 
+class ShippingAndInformationViewTest(TestCase):
+    """
+    Test the ShippingAndInformationView
+    1. Test that the view url exists at the desired location
+    and returns a 200 response code & uses the correct template
+    2. Test that the view returns the correct context data
+    """
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_context_data(self):
+        response = self.client.get(
+            reverse('customer_support:shipping-and-information'))
+
+        # Ensure status code 200 i.e., OK
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+                                'shipping_and_information.html')
+
+        self.assertIn('title', response.context_data)
+        self.assertIn('free_delivery_threshold', response.context_data)
+        self.assertEqual(response.context_data['title'],
+                         'Shipping & Information')
+        self.assertEqual(response.context_data['free_delivery_threshold'],
+                         50)
+
+
