14600f1bea985593dc597920425ceb229005de83
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-11-24 08:25:38 +0000

    test(views): add tests for Terms&Conditions view
    1. Added tests for the following
        - existence of the view url at the desired location
        - statusCode returned being 200
        - correct template usage
        - correct context data
    
    issue:#6

--- apps/customer_support/tests/test_views.py
+++ apps/customer_support/tests/test_views.py
@@ -170,3 +170,26 @@ class ShippingAndInformationViewTest(TestCase):
                          50)
 
 
+class TermsAndConditionsViewTest(TestCase):
+    """
+    Test the TermsAndConditionsView
+    1. Test that the view url exists at the desired location
+    and returns a 200 response code & uses the correct template
+    2. Test that the view returns the correct context data
+    """
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_context_data(self):
+        response = self.client.get(
+            reverse('customer_support:terms-and-conditions'))
+
+        # Ensure status code 200 i.e., OK
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+                                'terms_and_conditions.html')
+
+        self.assertIn('title', response.context_data)
+        self.assertEqual(response.context_data['title'],
+                         'Terms & Conditions')
+
